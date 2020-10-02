b64343acc69d60ba0648dd68def1cf7d9aedb6fe
Ryan <70819971+0x392@users.noreply.github.com>
2020-10-03 07:01:35 +0800

    Update list_helper.js
    Add function `favoriteBlog`

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -3,4 +3,18 @@ const dummy = (blogs) => 1;
 
 const totalLikes = (blogs) => blogs.reduce((sum, blog) => sum + blog.likes, 0);
 
-module.exports = { dummy, totalLikes };
+const favoriteBlog = (blogs) => {
+  let mostLikesCount = 0;
+  let mostLikesIdx = 0;
+
+  blogs.forEach((blog, i) => {
+    if (blog.likes > mostLikesCount) {
+      mostLikesCount = blog.likes;
+      mostLikesIdx = i;
+    }
+  });
+
+  return blogs.length === 0 ? null : blogs[mostLikesIdx];
+};
+
+module.exports = { dummy, totalLikes, favoriteBlog };

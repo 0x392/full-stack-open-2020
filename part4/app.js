ca4e6cf3ed0b2cad7bb3d411d410831cb94e65e6
Ryan <70819971+0x392@users.noreply.github.com>
2020-10-02 16:48:38 +0800

    Update app.js
    Extract routers

--- part4/app.js
+++ part4/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
+const blogsRouter = require("./controllers/blogs");
 const mongoose = require("mongoose");
-const Blog = require("./models/blog");
 const config = require("./utils/config");
 const logger = require("./utils/logger");
 const middleware = require("./utils/middleware");
@@ -20,19 +20,7 @@ mongoose
 app.use(express.json());
 app.use(middleware.requestLogger);
 
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
+app.use("/api/blogs", blogsRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);

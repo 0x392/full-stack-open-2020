93c5f76c5673e2c4ea62a516d66004fcdf97ea0d
Ryan <70819971+0x392@users.noreply.github.com>
2020-10-05 10:38:35 +0800

    Update blogs.js

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -21,4 +21,18 @@ blogsRouter.delete("/:id", async (request, response) => {
   response.status(204).end();
 });
 
+blogsRouter.put("/:id", async (request, response, next) => {
+  const { body } = request;
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+  };
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+    new: true,
+  });
+  response.json(updatedBlog);
+});
+
 module.exports = blogsRouter;

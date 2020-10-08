6eb1461134cbb0e98b3f24719ac83af297b35118
Ryan <70819971+0x392@users.noreply.github.com>
2020-10-08 12:26:25 +0800

    Rewrite and pass all the tests 💦

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -1,155 +1,29 @@
+const _ = require("lodash");
 const supertest = require("supertest");
 const app = require("../app");
 const helper = require("./test_helper");
-const mongoose = require("mongoose");
-const Blog = require("../models/blog");
 const User = require("../models/user");
+const Blog = require("../models/blog");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 
 const api = supertest(app);
 
-describe("there are initially some blog posts", () => {
-  beforeEach(async () => {
-    await Blog.deleteMany({});
-
-    const blogModels = helper.initialBlogs.map((blog) => new Blog(blog));
-    const promiseArray = blogModels.map((blog) => blog.save());
-    await Promise.all(promiseArray);
-  });
-
-  describe("Getting all blogs", () => {
-    // Exercise 4.8 - 1
-    test("returns correct amount of blogs", async () => {
-      const response = await api.get("/api/blogs");
-      expect(response.body).toHaveLength(helper.initialBlogs.length);
-    });
-
-    // Exercise 4.8 - 2
-    test("returns blogs in JSON format", async () => {
-      await api
-        .get("/api/blogs")
-        .expect(200)
-        .expect("Content-Type", /application\/json/);
-    });
-
-    // Exercise 4.9
-    test("returns blogs with property `id` and without `_id`", async () => {
-      const blogsAtStart = await helper.getBlogsInDb();
-      for (let i = 0; i < blogsAtStart.length; i++) {
-        let blog = blogsAtStart[i];
-        expect(blog.id).toBeDefined();
-        expect(blog._id).not.toBeDefined();
-      }
-    });
-  });
-
-  describe("Creating a new blog", () => {
-    // Exercise 4.10
-    test("successfully with a valid data", async () => {
-      const newBlog = {
-        title: "What's Happened, Happened",
-        author: "Neil",
-        url: "https://tenetquotes.com/neil",
-        likes: 11,
-      };
-
-      await api
-        .post("/api/blogs")
-        .send(newBlog)
-        .expect(201)
-        .expect("Content-Type", /application\/json/);
-
-      const blogsAtEnd = await helper.getBlogsInDb();
-      expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
-
-      const titles = blogsAtEnd.map((blog) => blog.title);
-      expect(titles).toContain(newBlog.title);
-    });
-
-    // Exercise 4.11
-    test("if `like` property is missing from the request, it defaults to 0", async () => {
-      const newBlog = {
-        title: "What's Happened, Happened",
-        author: "Neil",
-        url: "https://tenetquotes.com/neil",
-      };
-
-      const response = await api.post("/api/blogs").send(newBlog);
-      expect(response.body.likes).toBe(0);
-    });
-
-    // Exercise 4.12 - 1
-    test("responds with 400 if the `title` property is missing", async () => {
-      const newBlog = {
-        author: "Neil",
-        url: "https://tenetquotes.com/neil",
-      };
-
-      await api.post("/api/blogs").send(newBlog).expect(400);
-    });
-
-    // Exercise 4.12 - 2
-    test("responds with 400 if the `url` property is missing", async () => {
-      const newBlog = {
-        title: "What's Happened, Happened",
-        author: "Neil",
-      };
-
-      await api.post("/api/blogs").send(newBlog).expect(400);
-    });
-  });
-
-  describe("Deletion of a blog", () => {
-    test("succeeds with status code 204 when `id` is valid", async () => {
-      const blogAtStart = await helper.getBlogsInDb();
-      const blogToDelete = blogAtStart[0];
-      await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
-
-      const blogsAtEnd = await helper.getBlogsInDb();
-      expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1);
-
-      const titles = blogsAtEnd.map((blog) => blog.title);
-      expect(titles).not.toContain(blogToDelete.title);
-    });
-  });
-
-  describe("Update of a blog", () => {
-    test("succeeds with status code 200 for a valid request", async () => {
-      const blogAtStart = await helper.getBlogsInDb();
-      const blogToUpdate = blogAtStart[0];
-      blogToUpdate.title = "I'll See You in the Beginning";
-
-      await api
-        .put(`/api/blogs/${blogToUpdate.id}`)
-        .send(blogToUpdate)
-        .expect(200);
-
-      const updatedBlog = await Blog.findById(blogToUpdate.id);
-      expect(updatedBlog.title).toBe(blogToUpdate.title);
-    });
-  });
-});
-
-describe("Creating a new user", () => {
+describe("Creating a new user while there are some users in db", () => {
   beforeEach(async () => {
     await User.deleteMany({});
 
-    const userModels = helper.initialUsers.map((user) => new User(user));
-    const promiseArray = userModels.map((user) => user.save());
+    const userDocs = helper.mockUsers.map((mockUser) => new User(mockUser));
+    const promiseArray = userDocs.map((userDoc) => userDoc.save());
     await Promise.all(promiseArray);
   });
 
-  test("successfully with a valid data", async () => {
+  test("succeeds with valid data", async () => {
     const usersAtStart = await helper.getUsersInDb();
-
-    const newUser = {
-      username: "some_username",
-      password: "secret",
-      name: "Alice",
-    };
 
     await api
       .post("/api/users")
-      .send(newUser)
+      .send(helper.newUser)
       .expect(201)
       .expect("Content-Type", /application\/json/);
 
@@ -157,95 +31,266 @@ describe("Creating a new user", () => {
     expect(usersAtEnd).toHaveLength(usersAtStart.length + 1);
 
     const usernames = usersAtEnd.map((user) => user.username);
-    expect(usernames).toContain(newUser.username);
+    expect(usernames).toContain(helper.newUser.username);
   });
 
-  test("fails with status code 400 and proper message if username is not given", async () => {
-    const newUser = {
-      password: "secret",
-      name: "Alice",
-    };
+  test("fails with status code 400 and error message if `username` is not given", async () => {
+    const newUser = _.clone(helper.newUser);
+    delete newUser.username;
 
-    const result = await api
+    const response = await api
       .post("/api/users")
       .send(newUser)
       .expect(400)
       .expect("Content-Type", /application\/json/);
 
-    expect(result.body.error).toContain("`username` is required");
+    expect(response.body.error).toContain("`username` is required");
   });
 
-  test("fails with status code 400 and proper message if username is less than 3 characters", async () => {
-    const newUser = {
-      username: "hi",
-      password: "secret",
-      name: "Alice",
-    };
+  test("fails with status code 400 and error message if `username` is less than 3 characters", async () => {
+    const newUser = _.clone(helper.newUser);
+    newUser.username = "a";
 
-    const result = await api
+    const response = await api
       .post("/api/users")
       .send(newUser)
       .expect(400)
       .expect("Content-Type", /application\/json/);
 
-    expect(result.body.error).toContain("username");
-    expect(result.body.error).toContain(
+    expect(response.body.error).toContain("username");
+    expect(response.body.error).toContain(
       "shorter than the minimum allowed length (3)"
     );
   });
 
-  test("fails with status code 400 and proper message if password is not given", async () => {
-    const newUser = {
-      username: "some_username",
-      name: "Alice",
-    };
+  test("fails with status code 400 and error message if `password` is not given", async () => {
+    const newUser = _.clone(helper.newUser);
+    delete newUser.password;
 
-    const result = await api
+    const response = await api
       .post("/api/users")
       .send(newUser)
       .expect(400)
       .expect("Content-Type", /application\/json/);
 
-    expect(result.body.error).toContain("`password` is required");
+    expect(response.body.error).toContain("`password` is required");
   });
 
-  test("fails with status code 400 and proper message if password is less than 3 characters", async () => {
-    const newUser = {
-      username: "some_username",
-      password: "hi",
-      name: "Alice",
-    };
+  test("fails with status code 400 and error message if `password` is less than 3 characters", async () => {
+    const newUser = _.clone(helper.newUser);
+    newUser.password = "a";
 
-    const result = await api
+    const response = await api
       .post("/api/users")
       .send(newUser)
       .expect(400)
       .expect("Content-Type", /application\/json/);
 
-    expect(result.body.error).toContain(
-      "password is shorter than the minimum allowed length (3)"
+    expect(response.body.error).toContain("password");
+    expect(response.body.error).toContain(
+      "shorter than the minimum allowed length (3)"
     );
   });
 
-  test("fails with status code 400 and proper message if username is already taken", async () => {
+  test("fails with status code 400 and error message if `username` has already been taken", async () => {
     const usersAtStart = await helper.getUsersInDb();
 
     const newUser = {
-      username: "username_1",
-      password: "secret",
-      name: "Alice",
+      username: helper.getRandomUser().username,
+      password: "new_password",
+      name: "new_name",
     };
 
-    const result = await api
+    const response = await api
       .post("/api/users")
       .send(newUser)
       .expect(400)
       .expect("Content-Type", /application\/json/);
 
-    expect(result.body.error).toContain("`username` to be unique");
+    expect(response.body.error).toContain("`username` to be unique");
 
     const usersAtEnd = await helper.getUsersInDb();
     expect(usersAtEnd).toHaveLength(usersAtStart.length);
+  });
+});
+
+describe("There are some blog posts and users in db", () => {
+  beforeEach(async () => {
+    let promiseArray;
+
+    await User.deleteMany({});
+
+    const userDocs = helper.mockUsers.map((mockUser) => new User(mockUser));
+    promiseArray = userDocs.map((userDoc) => userDoc.save());
+    await Promise.all(promiseArray);
+
+    await Blog.deleteMany({});
+
+    const blogDocs = helper.mockBlogs.map((mockBlog) => new Blog(mockBlog));
+    promiseArray = blogDocs.map((blogDoc) => blogDoc.save());
+    await Promise.all(promiseArray);
+  });
+
+  describe("Getting all blog posts", () => {
+    // Exercise 4.8 - 1
+    test("returns correct amount of blog posts", async () => {
+      const response = await api.get("/api/blogs");
+      expect(response.body).toHaveLength(helper.mockBlogs.length);
+    });
+
+    // Exercise 4.8 - 2
+    test("returns blog posts in JSON", async () => {
+      await api
+        .get("/api/blogs")
+        .expect(200)
+        .expect("Content-Type", /application\/json/);
+    });
+
+    // Exercise 4.9
+    test("returns blog posts with property `id` but without `_id`", async () => {
+      const response = await api
+        .get("/api/blogs")
+        .expect(200)
+        .expect("Content-Type", /application\/json/);
+
+      response.body.forEach((blog) => {
+        expect(blog.id).toBeDefined();
+        expect(blog._id).not.toBeDefined();
+      });
+    });
+  });
+
+  describe("Creating a new blog post", () => {
+    // Exercise 4.10 and 4.22
+    test("succeeds with a valid data", async () => {
+      const randomUser = helper.getRandomUser();
+
+      // Login
+      let response = await api.post("/api/login").send({
+        username: randomUser.username,
+        password: helper.getPassword(randomUser),
+      });
+
+      const token = response.body.token;
+      const decodedToken = await jwt.verify(token, process.env.SECRET);
+
+      // Create a new blog post
+      response = await api
+        .post("/api/blogs")
+        .send(helper.newBlog)
+        .set("Authorization", `bearer ${token}`);
+      const savedPost = response.body;
+
+      expect(savedPost.user.toString()).toBe(decodedToken.id);
+
+      const blogsAtEnd = await helper.getBlogsInDb();
+      expect(blogsAtEnd).toHaveLength(helper.mockBlogs.length + 1);
+
+      const titles = blogsAtEnd.map((blog) => blog.title);
+      expect(titles).toContain(helper.newBlog.title);
+    });
+
+    // Exercise 4.11 and 4.20
+    test("if `like` property is missing from the request, it defaults to 0", async () => {
+      const randomUser = helper.getRandomUser();
+
+      // Login
+      let response = await api.post("/api/login").send({
+        username: randomUser.username,
+        password: helper.getPassword(randomUser),
+      });
+
+      const token = response.body.token;
+
+      const newBlogWithoutLikes = _.clone(helper.newBlog);
+      delete newBlogWithoutLikes.likes;
+
+      response = await api
+        .post("/api/blogs")
+        .send(newBlogWithoutLikes)
+        .set("Authorization", `bearer ${token}`);
+
+      expect(response.body.likes).toBe(0);
+    });
+
+    // Exercise 4.12 - 1
+    test("responds with 400 if the `title` property is missing", async () => {
+      const newBlog = _.clone(helper.newBlog);
+      delete newBlog.title;
+
+      await api.post("/api/blogs").send(newBlog).expect(400);
+    });
+
+    // Exercise 4.12 - 2
+    test("responds with 400 if the `url` property is missing", async () => {
+      const newBlog = _.clone(helper.newBlog);
+      delete newBlog.url;
+
+      await api.post("/api/blogs").send(newBlog).expect(400);
+    });
+  });
+
+  describe("Deleting a blog post", () => {
+    test("succeeds with status code 204 with a valid `id`", async () => {
+      const blogsAtStart = await helper.getBlogsInDb();
+
+      const blogToDelete = helper.getRandomBlog();
+      const user = await User.findById(blogToDelete.user);
+
+      // Login
+      let response = await api.post("/api/login").send({
+        username: user.username,
+        password: helper.getPassword(user),
+      });
+
+      const token = response.body.token;
+
+      await api
+        .delete(`/api/blogs/${blogToDelete._id}`)
+        .set("Authorization", `bearer ${token}`)
+        .expect(204);
+
+      const blogsAtEnd = await helper.getBlogsInDb();
+      expect(blogsAtEnd).toHaveLength(blogsAtStart.length - 1);
+
+      const titles = blogsAtEnd.map((blog) => blog.title);
+      expect(titles).not.toContain(blogToDelete.title);
+    });
+  });
+
+  describe("Updating a blog post", () => {
+    test("succeeds with status code 200 for a valid request", async () => {
+      const blogsAtStart = await helper.getBlogsInDb();
+
+      const blogToUpdate = helper.getRandomBlog();
+      const user = await User.findById(blogToUpdate.user);
+
+      // Login
+      let response = await api.post("/api/login").send({
+        username: user.username,
+        password: helper.getPassword(user),
+      });
+
+      const token = response.body.token;
+
+      blogToUpdate.title = "update_title";
+      blogToUpdate.author = "update_author";
+      blogToUpdate.url = "update_url";
+      blogToUpdate.likes = blogToUpdate.likes + 100;
+
+      await api
+        .put(`/api/blogs/${blogToUpdate._id}`)
+        .send(blogToUpdate)
+        .set("Authorization", `bearer ${token}`)
+        .expect(200);
+
+      const blogsAtEnd = await helper.getBlogsInDb();
+      expect(blogsAtEnd).toHaveLength(blogsAtStart.length);
+
+      const updatedBlog = await Blog.findById(blogToUpdate._id);
+      const fields = ["title", "author", "url", "likes"];
+      expect(_.pick(updatedBlog, fields)).toEqual(_.pick(blogToUpdate, fields));
+    });
   });
 });
 
--- part4/tests/test_helper.js
+++ part4/tests/test_helper.js
@@ -1,81 +1,122 @@
+const _ = require("lodash");
 const Blog = require("../models/blog");
 const User = require("../models/user");
 
-const initialBlogs = [
+const mockUsers = [
   {
-    _id: "5a422a851b54a676234d17f7",
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
-    likes: 7,
-    __v: 0,
-  },
-  {
-    _id: "5a422aa71b54a676234d17f8",
-    title: "Go To Statement Considered Harmful",
-    author: "Edsger W. Dijkstra",
-    url:
-      "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 5,
-    __v: 0,
-  },
-  {
-    _id: "5a422b3a1b54a676234d17f9",
-    title: "Canonical string reduction",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-    likes: 12,
-    __v: 0,
-  },
-  {
-    _id: "5a422b891b54a676234d17fa",
-    title: "First class tests",
-    author: "Robert C. Martin",
-    url:
-      "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-    likes: 10,
-    __v: 0,
-  },
-  {
-    _id: "5a422ba71b54a676234d17fb",
-    title: "TDD harms architecture",
-    author: "Robert C. Martin",
-    url:
-      "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-    likes: 0,
-    __v: 0,
-  },
-  {
-    _id: "5a422bc61b54a676234d17fc",
-    title: "Type wars",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-    likes: 2,
-    __v: 0,
-  },
-];
-
-const initialUsers = [
-  {
-    _id: "5f7c9a44d24deda40a7e4d68",
-    username: "username_1",
+    _id: "5f7e5ec41c6025465c49aada",
+    blogs: ["5f7e5ec51c6025465c49aade", "5f7e5ec51c6025465c49aae0"],
+    username: "mock_username_1",
     passwordHash:
-      "$2b$10$3yZMTAanb8yuTzlyo1K/4uimecdI1pe2oCEF1PpoxWKt6cV/PnAF.",
-    name: "name_1",
+      "$2b$10$AYl0VUmnM/5UcGfqppTMCee56msTwNF7FiWk6y2HOo6.Ihbwy54Xu",
+    name: "mock_name_1",
+    __v: 2,
+  },
+  {
+    _id: "5f7e5ec41c6025465c49aadb",
+    blogs: [
+      "5f7e5ec41c6025465c49aadc",
+      "5f7e5ec41c6025465c49aadd",
+      "5f7e5ec51c6025465c49aadf",
+    ],
+    username: "mock_username_2",
+    passwordHash:
+      "$2b$10$x8t1l76Md2qCvZ8Z7eAi6ub2Z4CZfblqcOZ5GadT7P7p69JSuWM1W",
+    name: "mock_name_2",
+    __v: 3,
+  },
+];
+
+const mockBlogs = [
+  {
+    _id: "5f7e5ec41c6025465c49aadc",
+    likes: 2,
+    title: "mock_title_1",
+    author: "mock_user_1",
+    url: "https://mockurl1.io/",
+    user: "5f7e5ec41c6025465c49aadb",
+    __v: 0,
+  },
+  {
+    _id: "5f7e5ec41c6025465c49aadd",
+    likes: 3,
+    title: "mock_title_2",
+    author: "mock_user_2",
+    url: "https://mockurl2.io/",
+    user: "5f7e5ec41c6025465c49aadb",
+    __v: 0,
+  },
+  {
+    _id: "5f7e5ec51c6025465c49aade",
+    likes: 5,
+    title: "mock_title_3",
+    author: "mock_user_3",
+    url: "https://mockurl3.io/",
+    user: "5f7e5ec41c6025465c49aada",
+    __v: 0,
+  },
+  {
+    _id: "5f7e5ec51c6025465c49aadf",
+    likes: 7,
+    title: "mock_title_4",
+    author: "mock_user_4",
+    url: "https://mockurl4.io/",
+    user: "5f7e5ec41c6025465c49aadb",
+    __v: 0,
+  },
+  {
+    _id: "5f7e5ec51c6025465c49aae0",
+    likes: 11,
+    title: "mock_title_5",
+    author: "mock_user_5",
+    url: "https://mockurl5.io/",
+    user: "5f7e5ec41c6025465c49aada",
     __v: 0,
   },
 ];
 
-const getBlogsInDb = async () => {
-  const blogModels = await Blog.find({});
-  // Calling `toJSON` to remove `_id` and `__v` fields
-  return blogModels.map((blog) => blog.toJSON());
+const newUser = {
+  username: "new_username",
+  password: "new_password",
+  name: "new_name",
+};
+
+const newBlog = {
+  title: "new_title",
+  author: "new_author",
+  url: "https://newurl.io/",
+  likes: 8,
 };
 
 const getUsersInDb = async () => {
-  const userModels = await User.find({});
-  // Calling `toJSON` to remove `passwordHash`, `_id` and `__v` fields
-  return userModels.map((user) => user.toJSON());
+  const userDocs = await User.find({});
+
+  // Calls `toJSON` to remove `passwordHash`, `_id` and `__v` fields
+  return userDocs.map((userDoc) => userDoc.toJSON());
 };
 
-module.exports = { initialBlogs, getBlogsInDb, initialUsers, getUsersInDb };
+const getBlogsInDb = async () => {
+  const blogDocs = await Blog.find({});
+
+  // Calling `toJSON` to remove `_id` and `__v` fields
+  return blogDocs.map((blog) => blog.toJSON());
+};
+
+const getRandomUser = () => _.sample(mockUsers);
+
+const getRandomBlog = () => _.sample(mockBlogs);
+
+const getPassword = (user) =>
+  `mock_password_${_.last(user.username.split("_"))}`;
+
+module.exports = {
+  mockUsers,
+  mockBlogs,
+  getUsersInDb,
+  getBlogsInDb,
+  getRandomUser,
+  getRandomBlog,
+  getPassword,
+  newUser,
+  newBlog,
+};

71b4b70434d736fb37cd1e1a38cbf5ff5d58ba5d
Ryan <70819971+0x392@users.noreply.github.com>
2020-10-10 21:41:38 +0800

    Update Blog.js
    Add "Show detail" button

--- part5/src/components/Blog.js
+++ part5/src/components/Blog.js
@@ -1,8 +1,36 @@
-import React from "react";
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import React, { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const [showDetail, setShowDetail] = useState(false);
+
+  const blogStyle = {
+    border: "1px solid #000",
+    borderRadius: ".5rem",
+    marginTop: ".5rem",
+    padding: ".6rem",
+  };
+
+  const showWhenShowDetail = { display: showDetail ? "" : "none" };
+
+  const toggleShowDetail = () => setShowDetail(!showDetail);
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title}{" "}
+        <button onClick={toggleShowDetail}>
+          {!showDetail ? "Show Detail" : "Hide"}
+        </button>
+      </div>
+      <div style={showWhenShowDetail}>
+        <ul>
+          <li>URL: {blog.url}</li>
+          <li>Author: {blog.author}</li>
+          <li>Likes: {blog.likes} <button>Like</button></li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Blog;

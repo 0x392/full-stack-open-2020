b67a9764a5968c7054e18a1976c0f853f2f1aa1c
Ryan <70819971+0x392@users.noreply.github.com>
2020-10-18 17:29:44 +0800

    Create "ultimate-hooks"
    Exercise 7.8

---
+++ part7/ultimate-hooks/src/App.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { useField, useResource } from "./hooks";
+
+const App = () => {
+  const content = useField("text");
+  const name = useField("text");
+  const number = useField("text");
+
+  const [notes, noteService] = useResource("http://localhost:3005/notes");
+  const [persons, personService] = useResource("http://localhost:3005/persons");
+
+  const handleNoteSubmit = (event) => {
+    event.preventDefault();
+    noteService.create({ content: content.value });
+  };
+
+  const handlePersonSubmit = (event) => {
+    event.preventDefault();
+    personService.create({ name: name.value, number: number.value });
+  };
+
+  return (
+    <>
+      <h2>Notes</h2>
+      <form onSubmit={handleNoteSubmit}>
+        Content <input {...content} />
+        <br />
+        <button>Create</button>
+      </form>
+      {notes.map((n) => (
+        <p key={n.id}>{n.content}</p>
+      ))}
+
+      <h2>Persons</h2>
+      <form onSubmit={handlePersonSubmit}>
+        Name <input {...name} />
+        <br />
+        Number <input {...number} />
+        <br />
+        <button>Create</button>
+      </form>
+      {persons.map((n) => (
+        <p key={n.id}>
+          {n.name} {n.number}
+        </p>
+      ))}
+    </>
+  );
+};
+
+export default App;
